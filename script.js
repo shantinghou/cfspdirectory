64e3806df82dc669d5536644c0caa7539ad49ab2
Shanting <shantingh2021@student.cis.edu.hk>
2022-11-17 16:23:03 -0500

    header

---
+++ script.js
@@ -0,0 +1,10 @@
+var businesses = [{businessname:"weij"
+                    }]
+
+//tio's function filtering
+
+function displayCards(filteredbusinesses){
+    //loop through businesses
+    //create element
+    //add to child of div
+}
